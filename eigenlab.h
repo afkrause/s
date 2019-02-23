03525bcc1be6b490d311e0014d51977c688564f3
afk <andre_frank.krause@uni-bielefeld.de>
2019-02-23 11:00:59 +0100

    eigenlab : load_matrix

--- eigenlab.h
+++ eigenlab.h
@@ -60,3 +60,37 @@ template<class T, int d1, int d2, int d3, int d4, int d5> Matrix<T, d1, d2, d3, 
 */
 
 }
+
+template<class T> bool load_matrix(Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>& m, const string& fname)
+{
+	fstream f;
+	f.open(fname.c_str(), ios::in);
+	if(f.is_open())
+		return load_matrix(m, f);
+	else
+		return false;
+	return true;
+}
+
+template<class T> bool load_matrix(Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>& m, istream& is)
+{
+	T x;
+	
+	for (int r=0; r<m.rows();r++)
+	{
+		for (int c=0;c<m.cols();c++)	
+		{
+			if(is.eof()) return false;
+			is >> x; 
+			m(r, c) = x;
+		}
+	}
+	return true;
+}
+
+
+
+
+
+
+
