b18a777c067c9a6d0e43eab0c9ac1987c5a239cd
afk <andre_frank.krause@uni-bielefeld.de>
2020-03-08 12:08:29 +0100

    buffer added to video writer class. this effectively avoids framedrops (if HDD is fast enough anyway)

--- cv_save_video.cpp
+++ cv_save_video.cpp
@@ -4,11 +4,31 @@
 
 void Save_video::write(cv::Mat& img)
 {
+	using namespace std;
+
+	const lock_guard<mutex> lock(mx);
+	
+	if (free_list.empty())
+	{
+		std::cerr << "\nframe dropped while writing to: " << video_file_name;
+	}
+	else
+	{
+		auto mat = free_list.back();
+		free_list.pop_back();
+		img.copyTo(*mat);
+		fifo.push_back(mat);
+	}
+	
+
+
+	/*
 	// videowriter has not finished yet (new_video_writer_frame not set to false) -- framedrop detected..
 	if (video_writer.isOpened() && new_frame == true)
 	{
 		std::cerr << "\nframe dropped while writing to: " << video_file_name;
 	}
+	
 
 	if (new_frame == false)
 	{
@@ -17,41 +37,89 @@ void Save_video::write(cv::Mat& img)
 		mx.unlock();
 		new_frame = true;
 	}
+	*/
 }
 
-void Save_video::open(std::string fname, int fps, cv::Size2i size, int codec)
+void Save_video::open(std::string fname, int fps, cv::Size2i size, int buffer_size, int codec)
 {
-	video_file_name = fname;
 	close();
+	free_list.clear();
+	fifo.clear();
+	buffer.resize(buffer_size);
+	for (auto& x : buffer) { free_list.push_back(&x); }
+	std::cout << "\nvideo writer buffer size=" << free_list.size();
+
+	video_file_name = fname;
+	
 	video_writer.open(fname, codec, fps, size);
-	new_frame = false;
-	local_thread = std::thread(&Save_video::run_thread, this);
+	run_thread = true;
+	local_thread = std::thread(&Save_video::video_writer_thread, this);
+}
+
+Save_video::~Save_video()
+{
+	close();
 }
 
 void Save_video::close()
 {
-	video_writer.release();
-	new_frame = false;
+	using namespace std;
+	using namespace chrono;
+
+	// write remaining frames to disk
+	bool frames_remaining = true;
+	while (frames_remaining)
+	{
+		mx.lock();
+		if (fifo.empty())
+		{
+			frames_remaining = false;
+		}
+		mx.unlock();
+		std::this_thread::sleep_for(100ms);
+	}
+	
+	// wait for thread to finish
+	run_thread = false;
 	if (local_thread.joinable())
 	{
-		local_thread.join(); // wait for thread to finish
+		local_thread.join(); 
 	}
+
+	// release video_writer and close timestamp fstream
+	video_writer.release();
+
+	// todo 
+	// fstream_timestamp.close();
 }
 
-
-void Save_video::run_thread()
+void Save_video::video_writer_thread()
 {
+	using namespace std;
 	using namespace std::chrono;
-	std::cout << "\nvideo writer thread started.";
-	while (video_writer.isOpened())
+	cout << "\nvideo writer thread started.";
+
+	while (run_thread)
 	{
-		if (new_frame)
+		cv::Mat* mat = nullptr;
+		mx.lock();
+		if (!fifo.empty())
 		{
-			mx.lock();
-			video_writer.write(frame);
-			mx.unlock();
-			new_frame = false;
+			mat = fifo.front();
+			fifo.erase(fifo.begin());
 		}
+		mx.unlock();
+
+		if (mat != nullptr)
+		{
+			video_writer.write(*mat);
+
+			mx.lock();
+			free_list.push_back(mat);
+			// debug cout << "\nfree_list size = " << free_list.size();
+			mx.unlock();			
+		}
+
 		std::this_thread::sleep_for(1ms);
 		cv::waitKey(1); // needed for event propagation (or not needed??)
 	}
--- cv_save_video.h
+++ cv_save_video.h
@@ -14,10 +14,17 @@ protected:
 	std::thread local_thread;
 	std::mutex mx;
 	std::string video_file_name;
-	void run_thread();
+
+	std::vector<cv::Mat> buffer;
+	std::vector<cv::Mat*> free_list;
+	std::vector<cv::Mat*> fifo;
+	std::atomic<bool> run_thread = true;
+	void video_writer_thread();
+	//std::atomic<bool> new_frame = false;
 public:
-	void open(std::string fname, int fps, cv::Size2i size, int codec = cv::VideoWriter::fourcc('M', 'J', 'P', 'G'));
-	void close();
-	std::atomic<bool> new_frame = false;
+	void open(std::string fname, int fps, cv::Size2i size, int buffer_size = 10, int codec = cv::VideoWriter::fourcc('M', 'J', 'P', 'G'));
+	void close();	
 	void write(cv::Mat& frame);
+
+	~Save_video();
 };
